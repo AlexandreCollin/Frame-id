c7b41e9a999a9b736277f5ed4adc683298c9202e
Alexandre Chauvin <alexandre.chauvin@epitech.eu>
2022-11-23 20:51:08 +0100

    add: headerMenu with add document button and icon add: grid with cards and hover effect in landing page

--- web/components/LandingPage.js
+++ web/components/LandingPage.js
@@ -2,7 +2,16 @@ import React from "react";
 
 // Components
 import NextLink from "next/link";
-import { Heading, Box, Image, Flex, Button, HStack, Text, Grid} from "@chakra-ui/react";
+import {
+  Heading,
+  Box,
+  Image,
+  Flex,
+  Button,
+  HStack,
+  Text,
+  SimpleGrid
+} from "@chakra-ui/react";
 import CustomCard from "../components/UI/CustomCard";
 import Section from "../components/UI/Section";
 
@@ -10,86 +19,51 @@ export default function HomePage() {
   return (
     <>
       <Flex p={12} bg="#1F2125" justify="space-between">
-       <HStack spacing={350}>
-        <Box>
-        <Flex>
-        <Text fontSize="4xl" color="#F6F5F6">
-          Welcome back,
-        </Text>
-        <Text ml={2} fontSize="4xl" fontWeight="bold" color="primary.500">{" "}Alexandre</Text>
-        </Flex>
-        
-        <Text fontSize="xl" color="#8C8E97" mt={2} ml={5}>
-          Here you can manage your documents, your identity and your security !
-        </Text>
-        </Box>
-        <Box>
-          <Text fontSize="2xl" color="#F6F5F6" mt={10} mb={5}>
-            Today's date
-          </Text>
-          <Text fontSize="xl" color="#8C8E97" mb={5}>
-            12/12/2021
-          </Text>
+          <Box>
+            <Flex fontSize={{base: "4xl", md: "6xl"}}>
+              <Text  color="#F6F5F6">
+                Welcome back,
+              </Text>
+              <Text ml={2}  fontWeight="bold" color="primary.500">
+                {" "}
+                Alexandre
+              </Text>
+            </Flex>
+
+            <Text fontSize="xl" color="#8C8E97" mt={2} ml={5}>
+              Here you can manage your documents, your identity and your
+              security !
+            </Text>
           </Box>
           <Box>
-          <Text fontSize="2xl" color="#F6F5F6" mt={10} mb={5}>
-            Your last connection
-          </Text>
-          <Text fontSize="xl" color="#8C8E97" mb={5}>
-            12/12/2021
-          </Text>
+            <Text fontSize="2xl" color="#F6F5F6" mt={10} mb={5}>
+              Your last connection
+            </Text>
+            <Text fontSize="xl" color="#8C8E97" mb={5}>
+              12/12/2021
+            </Text>
           </Box>
-          </HStack>
       </Flex>
-      <Box bg="primary.500" p={5}>
-        <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-          <CustomCard bgColor="#101010" maxW="4xl">
-            <Text color="white">Passeport</Text>
-          </CustomCard>
-          <CustomCard bgColor="#101010" maxW="4xl">
-            <Text color="white">CV</Text>
-          </CustomCard>
-          <CustomCard bgColor="#101010" maxW="4xl">
-            <Text color="white">truc</Text>
-          </CustomCard>
-          <CustomCard bgColor="#101010" maxW="4xl">
-            <Text color="white">OK DEDANS</Text>
-          </CustomCard>
-          <CustomCard bgColor="#101010" maxW="4xl">
-            <Text color="white">OK DEDANS</Text>
-          </CustomCard>
-          <CustomCard bgColor="#101010" maxW="4xl">
-            <Text color="white">OK DEDANS</Text>
-          </CustomCard>
-          <CustomCard bgColor="#101010" maxW="4xl">
-            <Text color="white">OK DEDANS</Text>
-          </CustomCard>
-          <CustomCard bgColor="#101010" maxW="4xl">
-            <Text color="white">OK DEDANS</Text>
-          </CustomCard>
-          <CustomCard bgColor="#101010" maxW="4xl">
-            <Text color="white">OK DEDANS</Text>
-          </CustomCard>
-          <CustomCard bgColor="#101010" maxW="4xl">
-            <Text color="white">OK DEDANS</Text>
-          </CustomCard>
-          <CustomCard bgColor="#101010" maxW="4xl">
-            <Text color="white">OK DEDANS</Text>
-          </CustomCard>
-          <CustomCard bgColor="#101010" maxW="4xl">
-            <Text color="white">OK DEDANS</Text>
-          </CustomCard>
-          <CustomCard bgColor="#101010" maxW="4xl">
-            <Text color="white">OK DEDANS</Text>
-          </CustomCard>
-          <CustomCard bgColor="#101010" maxW="4xl">
-            <Text color="white">OK DEDANS</Text>
-          </CustomCard>
-          <CustomCard bgColor="#101010" maxW="4xl">
-            <Text color="white">OK DEDANS</Text>
-          </CustomCard>
-          </Grid>
+      <Box bg="#101010" p={5}>
+        <SimpleGrid templateColumns={{base: "repeat(1, 1fr)", md: "repeat(3, 1fr)"}} gap={6}>
+          {Array.apply(null, Array(6)).map((_, i) => (
+          <CustomCard
+            key={i}
+            title="Documents"
+            description="Manage your documents"
+            icon="document"
+            link="/documents"
+            h={400}
+            _hover={{
+              transform: "scale(1.02)",
+              transition: "0.3s",
+              boxShadow: "md",
+            }}
+          />
+          ))}
+        </SimpleGrid>
+        
       </Box>
-      </>
+    </>
   );
 }
--- web/components/Layout/HeaderMenu.js
+++ web/components/Layout/HeaderMenu.js
@@ -1,14 +1,56 @@
 import React from "react";
 
 // Components
-import { Flex, Box, Image, Heading } from "@chakra-ui/react";
+import { Flex, Image, Avatar, Text, VStack, Button, Hide } from "@chakra-ui/react";
+
+//Icons
+import UploadIcon from "../Icon/UploadIcon";
 
 export default function HeaderMenu(props) {
   return (
-    <Box w="full" h={125} bgColor="#1F2125" borderBottom="1px solid" borderColor="#34363C">
-        <Heading size="2xl" color="#F6F5F6" p={8}>
-          Frame - ID
-        </Heading>
-    </Box>
+    <Flex
+      w="full"
+      h={100}
+      bgColor="#1F2125"
+      borderBottom="1px solid"
+      borderColor="#34363C"
+      p={8}
+      align="center"
+      justify="space-between"
+    >
+      <Image
+        src="/images/frame-id.png"
+        alt="FrameID"
+        w={155}
+        h={59}
+        justify="center"
+      />
+      <Flex align="center">
+        <Button
+          mr={5}
+          colorScheme="primary"
+          leftIcon={
+            <UploadIcon
+              w="auto"
+              h={35}
+              color="#1F2125"
+              />
+          }
+        >
+          New document
+        </Button>
+        <Avatar name="Antoine" mr={5} />
+        <Hide below="md">
+        <VStack display="block">
+          <Text fontWeight="medium" color="white">
+            Alexandre
+          </Text>
+          <Text fontSize="sm" color="gray.500">
+            Member since 5 days
+          </Text>
+        </VStack>
+        </Hide>
+      </Flex>
+    </Flex>
   );
 }
